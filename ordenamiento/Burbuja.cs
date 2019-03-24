7eff1d06dd94d5d1de4dad65eab7747b37346304
cesar magallon <cesar9mv@gmail.com>
2019-03-24 16:04:38 -0600

    Agregar proyecto de pruebas

--- ordenamiento/Burbuja.cs
+++ ordenamiento/Burbuja.cs
@@ -10,3 +10,14 @@ namespace ordenamiento
         }
     }
 }
+
+mkdir ordenamiento.pruebas
+cd ordenamiento.pruebas
+dotnet new mstest
+dotnet add reference + path de ordenamiento
+cd ..
+dotnet sln add ordenamiento.pruebas
+agregar cambios despues
+commit "agregar proyecto de pruebas"
+renombrar el de test por pruebasOrdenamiento
+despues de poner la clase y todo ejecutar comando test
