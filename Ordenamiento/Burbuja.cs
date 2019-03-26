0361062a610012d0485a3e27e7c2494296b974fd
cesar magallon <cesar9mv@gmail.com>
2019-03-26 07:31:46 -0600

    Arreglar la clase ordenamiento

--- Ordenamiento/Burbuja.cs
+++ Ordenamiento/Burbuja.cs
@@ -2,7 +2,20 @@
 
 namespace Ordenamiento
 {
-    public class Burbuja
-    {
+    public class Burbuja {
+        public void Ordenar(int[] arr) {
+             for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = arr.Length - 1; j >= i; j--)
+                {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        int aux = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = aux;
+                    }
+                }
+            }
+        }
     }
 }
